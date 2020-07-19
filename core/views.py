5a63aa05fb094754bf62249a7aeee1a253d84b1e
safwanvk <safwanvalakundil@gmail.com>
2020-07-19 14:09:27 +0530

    :white_check_mark: Set global summary

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
+import requests
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    result = None
+    data = True
+    global_summary = None
+    while(data):
+        try:
+            result = requests.get('https://api.covid19api.com/summary')
+            global_summary = result.json()['Global']
+            data = False
+        except:
+            data = True
+    context = {'result': result, 'global_summary': global_summary}
+    return render(request, 'index.html', context)
