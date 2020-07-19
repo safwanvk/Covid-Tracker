1fa3ecb6c43d90cccc7614984bfce0a988d2216f
safwanvk <safwanvalakundil@gmail.com>
2020-07-19 15:00:15 +0530

    :white_check_mark: Set Country wise

--- core/views.py
+++ core/views.py
@@ -7,12 +7,15 @@ def index(request):
     result = None
     data = True
     global_summary = None
+    countries = None
     while(data):
         try:
             result = requests.get('https://api.covid19api.com/summary')
-            global_summary = result.json()['Global']
+            json = result.json()
+            global_summary = json['Global']
+            countries = json['Countries']
             data = False
         except:
             data = True
-    context = {'result': result, 'global_summary': global_summary}
+    context = {'result': result, 'global_summary': global_summary, 'countries': countries}
     return render(request, 'index.html', context)
